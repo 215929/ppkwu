7829855a87b9de1e0f9cc78ef04604461d3016c1
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-10-13 14:35:34 +0200

    endpoint is now displayed

--- ppkwu1/rev.py
+++ ppkwu1/rev.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/<word_to_reverse>')
+def hello(word_to_reverse):
+    return word_to_reverse
 
 app.run()
+
