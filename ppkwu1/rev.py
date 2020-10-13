fc09200602007c8021bab43fd349888ec6de6ae2
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-10-13 15:01:26 +0200

    added a more descriptive route

--- ppkwu1/rev.py
+++ ppkwu1/rev.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/<word_to_reverse>')
+@app.route('/ppkw/rev/<word_to_reverse>')
 def hello(word_to_reverse):
     return word_to_reverse[::-1]
 
