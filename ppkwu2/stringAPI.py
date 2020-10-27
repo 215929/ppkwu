a07d6de0296a5f060a2bedd603b6cd50a5ac319f
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-10-27 14:32:11 +0100

    implemented function to detect lowercase

--- ppkwu2/stringAPI.py
+++ ppkwu2/stringAPI.py
@@ -3,9 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def check_for_lowercase(string_to_check):
+    for char in string_to_check:
+        if char.islower():
+            return True
+    return False
+
+
 @app.route('/<string_to_check>')
 def hello_world(string_to_check):
-    return string_to_check
+    if check_for_lowercase(string_to_check):
+        return "true"
+    return "false"
 
 
 app.run()
