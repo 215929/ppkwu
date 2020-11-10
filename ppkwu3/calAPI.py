ee7f1f2130cdbfa2b7872da3155abc0d45cdeb7d
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-11-10 15:15:17 +0100

    researching options for parsing an html

--- ppkwu3/calAPI.py
+++ ppkwu3/calAPI.py
@@ -1,14 +1,21 @@
 from flask import Flask
 import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    response = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
-    return response.text
-    #return "Hello"
+    response = requests.get(
+        "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
+    return_value = ""
+
+    parsed_html = BeautifulSoup(response.text)
+    return_value = parsed_html.find_all('a')
+
+    return str(return_value)
+    # return "Hello"
 
 
 app.run()
