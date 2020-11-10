1c0948f01a7a0c21f45ff8669a2ad410259250e2
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-11-10 14:48:21 +0100

    displaying API response

--- ppkwu3/calAPI.py
+++ ppkwu3/calAPI.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     response = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
-    return str(response.status_code())
+    return response.text
     #return "Hello"
 
 
