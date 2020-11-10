4cafc45483e0643805146a47b0f8adc939503568
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-11-10 15:45:27 +0100

    implemented html parsing for events

--- ppkwu3/calAPI.py
+++ ppkwu3/calAPI.py
@@ -9,26 +9,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # response = requests.get(
-    #     "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
-    return_value = ""
+    response = requests.get(
+        "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
+    return_value = dict()
+
+    parsed_html = BeautifulSoup(response.text)
+
+    for link in parsed_html.find_all('a', {"class": "active"}):
+        return_value[link] = link.get('href')
+
+    #return_value = parsed_html.find_all('a')
+
+    # cal = Calendar()
     #
-    # parsed_html = BeautifulSoup(response.text)
-    # return_value = parsed_html.find_all('a')
-
-    cal = Calendar()
-
-    event = Event()
-
-    event.add('summary', "test")
-    event.add('dtstart', datetime.date(2020, 10, 10))
-    event.add('dtend', datetime.date(2020, 10, 10))
-
-    cal.add_component(event)
-
-    f = open('test.ics', 'wb')
-    f.write(cal.to_ical())
-    f.close()
+    # event = Event()
+    #
+    # event.add('summary', "test")
+    # event.add('dtstart', datetime.date(2020, 10, 10))
+    # event.add('dtend', datetime.date(2020, 10, 10))
+    #
+    # cal.add_component(event)
+    #
+    # f = open('test.ics', 'wb')
+    # f.write(cal.to_ical())
+    # f.close()
 
     return str(return_value)
     # return "Hello"
