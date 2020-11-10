5d4aee5d8903d2b0171c0b231f500c91d25cea95
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-11-10 15:30:38 +0100

    researching options for creating an ics file

--- ppkwu3/calAPI.py
+++ ppkwu3/calAPI.py
@@ -1,18 +1,34 @@
 from flask import Flask
 import requests
 from bs4 import BeautifulSoup
+from icalendar import Calendar, Event
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    response = requests.get(
-        "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
+    # response = requests.get(
+    #     "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
     return_value = ""
+    #
+    # parsed_html = BeautifulSoup(response.text)
+    # return_value = parsed_html.find_all('a')
 
-    parsed_html = BeautifulSoup(response.text)
-    return_value = parsed_html.find_all('a')
+    cal = Calendar()
+
+    event = Event()
+
+    event.add('summary', "test")
+    event.add('dtstart', datetime.date(2020, 10, 10))
+    event.add('dtend', datetime.date(2020, 10, 10))
+
+    cal.add_component(event)
+
+    f = open('test.ics', 'wb')
+    f.write(cal.to_ical())
+    f.close()
 
     return str(return_value)
     # return "Hello"
