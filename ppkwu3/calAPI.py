3dbc3c50937355a1e9120c10382d39f4deb030bd
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-11-10 14:47:40 +0100

    connecting to the WEEIA calendar API

--- ppkwu3/calAPI.py
+++ ppkwu3/calAPI.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    response = requests.get("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
+    return str(response.status_code())
+    #return "Hello"
 
 
 app.run()
