0376088bf36498f51812fc8b42c540e9ec419641
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-11-24 14:30:09 +0100

    implemented creating ics files from parsed response from WEEIA API

--- ppkwu3/calAPI.py
+++ ppkwu3/calAPI.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello():
     response = requests.get(
         "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1")
-    return_value = dict()
+    events = dict()
     event_list = []
 
     parsed_html = BeautifulSoup(response.text)
@@ -19,27 +19,27 @@ def hello():
     event_list = parsed_html.find_all('div', {"class": "InnerBox"})
 
     for day in parsed_html.find_all('a', {"class": "active"}):
-        return_value[day.getText()] = [day.get('href'), event_list.pop(0)]
+        events[day.getText()] = [day.get('href'), event_list.pop(0).text]
 
+    cal = Calendar()
 
-    #return_value = parsed_html.find_all('a')
+    return_value = []
 
-    # cal = Calendar()
-    #
-    # event = Event()
-    #
-    # event.add('summary', "test")
-    # event.add('dtstart', datetime.date(2020, 10, 10))
-    # event.add('dtend', datetime.date(2020, 10, 10))
-    #
-    # cal.add_component(event)
-    #
-    # f = open('test.ics', 'wb')
-    # f.write(cal.to_ical())
-    # f.close()
+    for day in events:
+        #return_value.append(events[day][1])
+
+        event = Event()
+
+        event.add('summary', events[day][1])
+        event.add('dtstart', datetime.date(2020, 10, int(day)))
+
+        cal.add_component(event)
+
+    f = open('test.ics', 'wb')
+    f.write(cal.to_ical())
+    f.close()
 
     return str(return_value)
-    # return "Hello"
 
 
 app.run()
